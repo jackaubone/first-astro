034dce53917ec4bd0d9c1fa7647f897528ba9e89
jack aubone <jfaubone@gmail.com>
2026-03-20 18:01:54 -0500

    Adds dark mode style with js toggle, incorporates a preact island for a greeting script toggled by a button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jackaubone.github.io',
-    base: '/first-astro',    
+  site: 'https://jackaubone.github.io',
+  base: '/first-astro',
+  integrations: [preact()],
 });
 
 // domain is https://jackaubone.github.io/first-astro/
